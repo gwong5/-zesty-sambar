f31152a93cd355290f9c530dc3b915a0de7c0544
John Hallman <jhallman5@Johns-MacBook-Pro.local>
2016-12-01 15:44:52 -0800

    library additions

---
+++ library.js
@@ -0,0 +1,8 @@
+Object.prototype.forEach = require(./forEach);
+Array.prototype.nth = require(./nth);
+String.prototype.repeat = require(./repeat);
+String.prototype.toLowerCase = require(./toLowerCase);
+String.prototype.capitalize = require(./capitalize);
+Array.prototype.take = require(./take);
+Function.prototype.once = require(./once);
+Object.prototype.map = require(./map);
