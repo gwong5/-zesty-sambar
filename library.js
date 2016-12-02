17157a5774238aab1eb7c35afa778b7a35ba7084
John Hallman <jhallman5@Johns-MacBook-Pro.local>
2016-12-01 16:52:44 -0800

    function library

--- library.js
+++ library.js
@@ -1,4 +1,4 @@
-Object.prototype.forEach = require(./forEach);
+funtion (){Object.prototype.forEach = require(./forEach);
 Array.prototype.nth = require(./nth);
 String.prototype.repeat = require(./repeat);
 String.prototype.toLowerCase = require(./toLowerCase);
@@ -6,3 +6,4 @@ String.prototype.capitalize = require(./capitalize);
 Array.prototype.take = require(./take);
 Function.prototype.once = require(./once);
 Object.prototype.map = require(./map);
+}();
