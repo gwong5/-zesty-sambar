9eb166fc9787d8000a6b9ff3d3f80248d2798d3c
Greg <kousagi1012@yahoo.com>
2016-12-05 08:32:07 -0800

    finished take_spec

--- test/take_spec.js
+++ test/take_spec.js
@@ -1,9 +1,9 @@
 const {take} = require('../take');
 const {expect} = require('chai');
 
-describe('take()', function() {
-  it('', function(){
-    var array = []
-    expect(take(array, 3)).to.equal("")
+describe('take() Returns a sliced array to the specified number of elements', function() {
+  it('Should return given array to element X', function(){
+    var array = [2, 3, 1, 5]
+    expect(take(array, 3)).to.equal("[2, 3, 1]")
   })
 })
